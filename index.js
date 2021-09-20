e129fd838263fafb9218b37b28637c7a3d7ce612
Long <thaihothanhlong95@gmail.com>
2021-09-20 17:56:34 +0700

    change-js-code

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+
+
+//URL API
+const url = 'https://tony-json-server.herokuapp.com/api/todos'
+
+
+function loadTracker () {
+  getTrackers((trackers)=> {
+    renderTracker(trackers)
+  })
+}
+
+loadTracker()
+
+
+
+
+// GET API TRACKER 
+
+function getTrackers (callback) {
+  fetch(url)
+  .then(res => res.json())
+  .then(callback)
+}
+
+// Render tracker 
+
+function renderTracker(trackers){
+  const allTrackers = trackers.data
+  const listTracker = document.getElementById('wrapper-list')
+  const htmls = allTrackers.map(tracker => {
+    return `
+    <div class="list-tracker">
+              <div class="list-tracker-header">
+                <h3 class="tracker-code" id="tracker-id">${tracker.id}</h3>
+                <span class="tracker-tag new-tag" id="tracker-status"
+                  >${tracker.status}</span
+                >
+              </div>
+              <div class="tracker-info">
+                <div class="description-info">
+                  <p class="description" id="description">${tracker.description}</p>
+                  <span class="tracker-tag tag" id="tag">${tracker.severity}</span>
+                </div>
+                <div id="option-button" class="option-button">
+                  <button id="close" class="btn clo-btn">Close</button>
+                  <button id="delete" class="btn del-btn" onclick ="deleteTracker(${tracker.id})">Delete</button>
+                </div>
+              </div>
+            </div>
+    `
+  })
+  listTracker.innerHTML = htmls
+}
+
+// Add Tracker
 const select = document.getElementById('select')
 const getValueSelect = () => {
   return select.options[select.selectedIndex].value
@@ -5,47 +61,13 @@ const getValueSelect = () => {
 
 select.addEventListener('change', getValueSelect)
 
-// GET API
-
-const url = 'https://tony-json-server.herokuapp.com/api/todos'
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const users = data.data
-    users.forEach((user) => getUser(user))
-  })
-
-function getUser(user) {
-  const listTracker = document.getElementById('wrapper-list')
-  listTracker.innerHTML += `
-        <div class="list-tracker">
-          <div class="list-tracker-header">
-            <h3 class="tracker-code" id="tracker-id">${user.id}</h3>
-            <span class="tracker-tag new-tag" id="tracker-status"
-              >${user.status}</span
-            >
-          </div>
-          <div class="tracker-info">
-            <div class="description-info">
-              <p class="description" id="description">${user.description}</p>
-              <span class="tracker-tag tag" id="tag">${user.severity}</span>
-            </div>
-            <div id="option-button" class="option-button">
-              <button id="close" class="btn clo-btn">Close</button>
-              <button id="delete" class="btn del-btn" onclick ="handleDeleteTracker()">Delete</button>
-            </div>
-          </div>
-        </div>
-  `
+function handleTrackerSubmit(e) {
+  e.preventDefault()
+  addTracker()
 }
 
-// ADD TRACKER List
-const formSubmit = document.getElementById('form')
-
-function handleTrackerSubmit(e) {
+function addTracker() {
   const des = document.getElementById('description').value
-  e.preventDefault()
   fetch(url, {
     method: 'POST',
     headers: {
@@ -58,35 +80,29 @@ function handleTrackerSubmit(e) {
       status: 'New',
     }),
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success', data)
-    })
-    .catch((error) => {
-      console.log('Error', error)
-    })
+  .then((response) => response.json())
+  .then(loadTracker)
+  .catch((error) => {
+    console.log('Error', error)
+  })
+  
+  des.value = ''
 }
+
+
+const formSubmit = document.getElementById('form')
 
 formSubmit.addEventListener('submit', handleTrackerSubmit)
 
 // DELETE TRACKER
-const getIdUser = () => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const users = data.data
-      users.forEach((user) => user.id)
-    })
-}
-console.log(getIdUser)
 
-function handleDeleteTracker() {
-  fetch(url / { getIdUser }, {
+function deleteTracker(id) {
+  fetch(url + '/' + id, {
     method: 'DELETE',
     headers: {
-      'Content-Type': 'application/json',
-    },
+      'Content-Type': 'application/json'
+    }
   })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
+  .then(res => res.json())
+  .then(loadTracker)
 }
