46cd969fffab628bff81c1471744f6bf38186744
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-10-09 17:25:54 +0700

    loading

--- register&login/login.js
+++ register&login/login.js
@@ -1,27 +1,42 @@
 const url = 'https://tony-json-server.herokuapp.com/api/users'
 const form = document.getElementById('form')
 const email = document.getElementById('email')
-const password = document.getElementById('password')
+const password = document.getElementById('password');
+const loading = document.getElementById('loading');
+loading.style.display = 'none';
+
 function getUsers(cb) {
   fetch(url)
-    .then((res) => res.json())
+    .then((res) => {
+      return res.json();
+    })
     .then(cb)
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+let users = [];
 
-  getUsers((data) => {
-    const allData = data.data
-    allData.forEach((user) => {
-      if (user.email === email.value && user.password === password.value) {
-        localStorage.setItem('user', email.value)
-        window.location.href = '../dashboard/index.html'
-      }
-    })
+getUsers(data => users = data.data)
+
+form.addEventListener('submit', (e) => {
+  loading.style.display = 'block';
+
+  e.preventDefault()
+  users.forEach((user) => {
+    if (user.email === email.value && user.password === password.value) {
+      localStorage.setItem('user', email.value)
+      window.location.href = '../dashboard/index.html'
+     
+    }
   })
+  
+  setTimeout(() => {
+    loading.style.display = 'none';
+  }, 500)
+  
 })
 
 if (localStorage.getItem('user') !== null) {
   window.location.href = '../dashboard/index.html'
 }
+
+
